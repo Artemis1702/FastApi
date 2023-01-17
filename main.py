d0ba40d9e9cfeeac5ababe4c4cd1e55ae2750547
sreyasqb <s.shreyas303@gmail.com>
2023-01-17 14:06:03 +0530

    new commit

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-from typing import Union
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/blog")
+def index(limit = 10, published: bool = True, sort: Optional[str] = None):
+    if(published):
+        return {'data' : f'{limit} published blogs from db'}
+    else:
+        return {'data' : f'{limit} blogs from db'}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+
+@app.get("/blog/{id}")
+def show(id: int):
+    return {"data": id}
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published : Optional[bool]
+
+
+@app.post("/blog")
+def create_blog(request: Blog):
+    return {"data" : f"Blog is created with title as {request.title}"}
